739f7b3e3fc5f221f3d3acbfc4f8fafe5cfc3a58
root <asanchez@neurowork.net>
2016-02-14 01:07:39 +0000

    Removed file

--- manifests/dependencies.pp
+++
@@ -1,7 +0,0 @@
-class dependencies {
-	package { 'cmake': ensure => 'installed' }
-	package { 'gcc': ensure => 'installed' }
-	package { 'make': ensure => 'installed' }
-	package { 'pkg-kde-tools': ensure => 'installed' }
-	package { 'python': ensure => 'installed' }
-}
