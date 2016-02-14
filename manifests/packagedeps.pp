0e1de384148e88afc4e12a10ceb47739a30088a6
root <asanchez@neurowork.net>
2016-02-14 18:30:42 +0000

    Added Redhat support

--- manifests/packagedeps.pp
+++ manifests/packagedeps.pp
@@ -4,20 +4,33 @@
 class packagedeps {
 	# Install dependencies (cmake, gcc, g++, python..)
 	package { 'cmake': ensure => 'installed' }
-	package { 'gcc': ensure => 'installed' }
-	package { 'g++': ensure => 'installed' }
 	package { 'make': ensure => 'installed' }
-	package { 'pkg-kde-tools': ensure => 'installed' }
-	package { 'pkg-config': ensure => 'installed' }
-	package { 'libzzip-dev': ensure => 'installed' }
-	package { 'python': ensure => 'installed' }
-	package { 'python-dev': ensure => 'installed' }
-	package { 'python-pip': ensure => 'installed' }
+	
+	if $::osfamily == 'Ubuntu' {
+		package { 'gcc': ensure => 'installed' }
+		package { 'g++': ensure => 'installed' }
+		package { 'pkg-kde-tools': ensure => 'installed' }
+		package { 'pkg-config': ensure => 'installed' }
+		package { 'libzzip-dev': ensure => 'installed' }
+		package { 'libzip-dev': ensure => 'installed' }
+		package { 'python': ensure => 'installed' }
+		package { 'python-dev': ensure => 'installed' }
+		package { 'python-pip': ensure => 'installed' }
+		$python = Package['python-dev']
+	} elsif $::osfamily == 'RedHat' {
+		package { 'gcc-c++': ensure => 'installed' }
+		package { 'zlib-devel': ensure => 'installed' }
+		package { 'python': ensure => 'installed' }
+		package { 'python-devel': ensure => 'installed' }
+		package { 'python-pip': ensure => 'installed' }
+		package { 'minizip-devel': ensure => 'installed' }
+		$python = Package['python-devel']
+	}
 
 	# Python dependency (python-dev required)
 	exec { 'numpy':  
     		command => '/usr/bin/pip install numpy',
 		timeout => 0,
-		require => Package['python-dev']
+		require => $python
 	}
 }
